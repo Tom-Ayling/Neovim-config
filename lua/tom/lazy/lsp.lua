cb256a50f9b6a39b8e168f28a9180c619b9b096c
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-09 20:30:40 +0000

    added autoformat on save to lsp

--- lua/tom/lazy/lsp.lua
+++ lua/tom/lazy/lsp.lua
@@ -29,13 +29,30 @@ return {
                 },
             }
         },
-
         config = function()
             vim.lsp.enable({ 'lua_ls', 'pyright' })
 
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ctions' })
-        end
 
-    }
+            -- setup autoformat on save
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then return end
+
+                    if client:supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            group = vim.api.nvim_create_augroup('my.lsp.format', { clear = false }),
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 1000 })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
 }
