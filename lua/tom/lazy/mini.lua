6387548317fb3325909ffe1de495c6d5c723ee13
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-09 14:08:07 +0000

    added mini git and diff

--- lua/tom/lazy/mini.lua
+++ lua/tom/lazy/mini.lua
@@ -2,13 +2,24 @@ return{
     { 'nvim-mini/mini.nvim',
         version = false ,
         config = function()
+
+            require ('mini.git').setup()
+            require('mini.diff').setup({
+                view = {
+                    style = 'sign',
+                    signs = { add = '++', change = '~~', delete = '--' },
+                },
+            })
+
             local statusline = require 'mini.statusline'
             statusline.setup {use_icons = true}
             statusline.section_location = function()
                 return '%2l:%-2v'
             end
+
             require ('mini.icons').setup()
-            require('mini.icons').mock_nvim_web_devicons()
+            require ('mini.icons').mock_nvim_web_devicons()
+
         end
     }
 
