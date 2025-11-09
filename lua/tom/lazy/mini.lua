eab8b8c55d5487400ff9381a1f0f1f6b85383bfb
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-09 20:29:41 +0000

    Added mini.comment

--- lua/tom/lazy/mini.lua
+++ lua/tom/lazy/mini.lua
@@ -22,6 +22,7 @@ return{
             require ('mini.surround').setup()
             require ('mini.splitjoin').setup()
             require ('mini.ai').setup()
+            require ('mini.comment').setup()
 
         end
     }
