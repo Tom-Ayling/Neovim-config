9990e1ad04ef455155011a2b996df1e428533a75
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-09 17:56:50 +0000

    changed themes to make use of opts instead of config

--- lua/tom/lazy/themes.lua
+++ lua/tom/lazy/themes.lua
@@ -3,29 +3,23 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-            })
-        end,
+        opts = {
+            flavour = "mocha",
+        },
     },
 
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        config = function()
-        end
+        opts = {},
     },
 
     {
         "folke/tokyonight.nvim",
         name = "tokyonight",
         priority = 1000,
-        config = function()
-            require("tokyonight").setup({
-                style = "night", 
-            })
-        end,
+        opts = {
+            style = "night",
+        },
     },
 }
-
