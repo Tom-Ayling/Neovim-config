01a454ee48a6646f845edc857e24c238b9e682a3
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-10 13:00:25 +0000

    added none-ls

--- lua/tom/lazy/lsp.lua
+++ lua/tom/lazy/lsp.lua
@@ -1,23 +1,24 @@
 return {
     {
-        'mason-org/mason.nvim',
+        "mason-org/mason.nvim",
         config = function()
-            require('mason').setup()
-        end
+            require("mason").setup()
+        end,
     },
     {
-        'mason-org/mason-lspconfig.nvim',
+        "mason-org/mason-lspconfig.nvim",
         config = function()
-            require('mason-lspconfig').setup({
+            require("mason-lspconfig").setup({
                 ensure_installed = {
-                    'lua_ls',
-                    'pyright'
-                }
+                    "lua_ls",
+                    "pyright",
+                },
             })
-        end
+            vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Open [m]ason" })
+        end,
     },
     {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         dependencies = {
             {
                 "folke/lazydev.nvim",
@@ -27,24 +28,26 @@ return {
                         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
                     },
                 },
-            }
+            },
         },
         config = function()
-            vim.lsp.enable({ 'lua_ls', 'pyright' })
+            vim.lsp.enable({ "lua_ls", "pyright" })
 
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ctions' })
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
 
             -- setup autoformat on save
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("my.lsp", { clear = true }),
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if not client then return end
+                    if not client then
+                        return
+                    end
 
-                    if client:supports_method('textDocument/formatting') then
-                        vim.api.nvim_create_autocmd('BufWritePre', {
-                            group = vim.api.nvim_create_augroup('my.lsp.format', { clear = false }),
+                    if client:supports_method("textDocument/formatting") then
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = vim.api.nvim_create_augroup("my.lsp.format", { clear = false }),
                             buffer = args.buf,
                             callback = function()
                                 vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 1000 })
