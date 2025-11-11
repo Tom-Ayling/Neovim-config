7d4528319093437e887afe57490ea023985b502a
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-11 13:28:35 +0000

    Changed settings for oil.nvim

--- lua/tom/lazy/oil.lua
+++ lua/tom/lazy/oil.lua
@@ -1,5 +1,14 @@
-return{
-  'stevearc/oil.nvim',
-  opts = {},
-  lazy = false,
+return {
+    "stevearc/oil.nvim",
+    opts = {},
+    lazy = false,
+    config = function()
+        require("oil").setup({
+            default_file_explorer = false,
+            delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+            },
+        })
+    end,
 }
