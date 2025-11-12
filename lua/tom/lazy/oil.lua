f0387b5f2cbd2a9590606ba20184ebb4476c9061
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-12 20:51:14 +0000

    skip simple edit confirmation for oil.nvim

--- lua/tom/lazy/oil.lua
+++ lua/tom/lazy/oil.lua
@@ -1,14 +1,15 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {},
-    lazy = false,
-    config = function()
-        require("oil").setup({
-            default_file_explorer = false,
-            delete_to_trash = true,
-            view_options = {
-                show_hidden = true,
-            },
-        })
-    end,
+	"stevearc/oil.nvim",
+	opts = {},
+	lazy = false,
+	config = function()
+		require("oil").setup({
+			default_file_explorer = false,
+			skip_confirm_for_simple_edits = true,
+			delete_to_trash = true,
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 }
