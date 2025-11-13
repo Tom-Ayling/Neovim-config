fd3ce6f3887bf8e37cc9c2c83961d037999966ef
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-13 16:01:27 +0000

    added black, and isort to ensure_installed

--- lua/tom/lazy/lsp.lua
+++ lua/tom/lazy/lsp.lua
@@ -222,7 +222,11 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
+			-- Lua
 			"stylua", -- Used to format Lua code
+			-- Python
+			"isort",
+			"black",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
