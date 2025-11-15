e249fbfa79791b14cc854d4cf6cdf3b9ef610c74
Tom-Ayling <Git@Tomayling.co.uk>
2025-11-15 01:32:21 +0000

    Added fugitive and gitsigns

--- lua/tom/lazy/mini.lua
+++ lua/tom/lazy/mini.lua
@@ -1,30 +1,28 @@
-return{
-    { 'nvim-mini/mini.nvim',
-        version = false ,
-        config = function()
+return {
+	{
+		"nvim-mini/mini.nvim",
+		version = false,
+		config = function()
+			-- require ('mini.git').setup()
+			-- require('mini.diff').setup({
+			--     view = {
+			--         style = 'sign',
+			--         signs = { add = '++', change = '~~', delete = '--' },
+			--     },
+			-- })
 
-            require ('mini.git').setup()
-            require('mini.diff').setup({
-                view = {
-                    style = 'sign',
-                    signs = { add = '++', change = '~~', delete = '--' },
-                },
-            })
+			local statusline = require("mini.statusline")
+			statusline.setup({ use_icons = true })
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
 
-            local statusline = require 'mini.statusline'
-            statusline.setup {use_icons = true}
-            statusline.section_location = function()
-                return '%2l:%-2v'
-            end
-
-            require ('mini.icons').setup()
-            require ('mini.icons').mock_nvim_web_devicons()
-            require ('mini.surround').setup()
-            require ('mini.splitjoin').setup()
-            require ('mini.ai').setup()
-            require ('mini.comment').setup()
-
-        end
-    }
-
+			require("mini.icons").setup()
+			require("mini.icons").mock_nvim_web_devicons()
+			require("mini.surround").setup()
+			require("mini.splitjoin").setup()
+			require("mini.ai").setup()
+			require("mini.comment").setup()
+		end,
+	},
 }
