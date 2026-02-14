225590a8b4d350b26a7b1aba1cc9c61d33f7a841
Tom-Ayling <Git@Tomayling.co.uk>
2026-02-14 15:55:06 +0000

    added luasnip

--- lua/tom/lazy/blink-cmp.lua
+++ lua/tom/lazy/blink-cmp.lua
@@ -3,7 +3,6 @@ return {
 		"saghen/blink.cmp",
 		-- optional: provides snippets for the snippet source
 		dependencies = {
-			"rafamadriz/friendly-snippets",
 			"erooke/blink-cmp-latex",
 			{ "saghen/blink.compat", version = "2.*", opts = {} },
 			{
